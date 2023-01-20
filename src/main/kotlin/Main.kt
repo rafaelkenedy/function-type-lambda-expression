bb7495ef5e95ef3067bd83723692cf4da2154172
rafael <k.rafaelalves@gmail.com>
2023-01-20 17:34:56 -0300

    challenge lambda

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,63 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    val trickFunction = trick
+//    trick()
+//    trickFunction()
+//    treat()
+
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters!"
+    }
+
+    val cupcake: (Int) -> String = {
+        "Have a cupcake!"
+    }
+
+    fun tips(): (Int) -> String = {
+        "$it quarters!"
+    }
+
+    fun cents(): (Int) -> String {
+        return { "$it quarters!" }
+    }
+
+    val treatFunction = trickOrTreat(false, coins)
+    val treatFunctionConcise = trickOrTreat(false, { "$it quarters" })
+    val treatFunctionFinal = trickOrTreat(false) { "$it quarters" }
+
+    val trickFunction = trickOrTreat(true, cupcake)
+    val trickFunctionNull = trickOrTreat(true, null)
+    //val treatFunctionB = trickOrTreat(true, tips())
+//    val trickFunction = trickOrTreat(true) {
+//        "$it quarters"
+//    }
+
+    //treatFunctionC()
+    //treatFunctionB()
+    treatFunction()
+    trickFunction()
+    trickFunctionNull()
+
+    repeat(4) {
+        treatFunctionFinal()
+    }
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    return if (isTrick) {
+        trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
 }
